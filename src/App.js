5f451353d7a4d1da8adbbce1762279ffc7c77903
RishiDesai17 <rishindesai@gmail.com>
2019-12-02 06:43:18 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React,{Component} from 'react';
 import Item from './Form/form';
-import List from './List/list';
 import './App.css';
-var des;
-var occ;
-var deadline;
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+let des;
+let occ;
+let deadline;
+
 class App extends Component {
   state = {
     items: []
@@ -21,39 +28,61 @@ class App extends Component {
       deadline=event.target.value;
     }
     else if(target.type==='submit'){
-      let item = {occ:'', des:'', deadline:''};
-      item.des = des;
-      item.occ = occ;
-      item.deadline = deadline;
-      let items = [
-        ...this.state.items
-      ];
-      items.push(item);
-      this.setState({items: items});
+      let item = {id:'', occ:'', des:'', deadline:''};
+      if(occ!==undefined && des!==undefined && deadline!==undefined) {
+        let id = Math.random()*100+1;
+        item.id = id;
+        item.des = des;
+        item.occ = occ;
+        item.deadline = deadline;
+        let items = [
+          ...this.state.items
+        ];
+        items.push(item);
+        this.setState({items: items});
+      }
+      else{
+        alert("Pls fill in all fields!");
+      }
     }
   }
-  delItem = (des) => {
+  delItem = (id) => {
     const itemIndex = this.state.items.findIndex(x => {
-      return x.des === des;
+      return x.id === id;
     });
     const items = [...this.state.items];
     items.splice(itemIndex, 1);
     this.setState({items: items});
   }
   render(){
-    let items = (
-        <div>
-        {this.state.items.map((item) => {
-          return <List occ={item.occ} des={item.des} deadline={item.deadline}
-            del={() => {this.delItem(this.state.items.des)}}/>
-        })}
-        </div>
-      );
     return (
       <div className="App">
         <Item submit={(event) => {this.addItem(event)}}
          />
-        {items}
+        <Paper className={{ width: '100%', overflowX: 'auto'}}>
+      <Table className={{minWidth: 650}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Occasion</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Deadline</TableCell>
+            <TableCell>X</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.state.items.map((item) => (
+            <TableRow >
+              <TableCell component="th" scope="row">
+                {item.occ}
+              </TableCell>
+              <TableCell>{item.des}</TableCell>
+              <TableCell>{item.deadline}</TableCell>
+              <TableCell><Button color="secondary" onClick={() => this.delItem(item.id)}>DELETE!</Button></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
       </div>
     );
   }
--- src/Form/form.js
+++ src/Form/form.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
 const Item = (props) => {
   return (
-  <div>
-    <p>Description:</p>
-    <input name="des" type="text" onChange={props.submit}/>
-    <p>Occasion:</p>
-    <input name="occ" type="text" onChange={props.submit}/>
-    <p>Deadline:</p>
-    <input name="deadline" type="text" onChange={props.submit}/>
-    <button type="submit" onClick={props.submit}>Submit!</button>
-  </div>
+    <div style={{paddingBottom: 70, paddingTop: 60}}>
+    <Grid container>
+      <Grid item xs={3}>
+        <TextField label="Occasion" variant="outlined" name="occ" onChange={props.submit}/>
+      </Grid>
+      <Grid item xs={3}>
+        <TextField label="Description" variant="outlined" name="des" onChange={props.submit}/>
+      </Grid>
+      <Grid item xs={3}>
+        <TextField label="Deadline" variant="outlined" name="deadline" onChange={props.submit}/>
+      </Grid>
+      <Grid item xs={3}>
+        <button type="submit" onClick={props.submit}>SUBMIT!</button>
+      </Grid>
+    </Grid>
+    </div>
+  // <div>
+  //   <p>Description:</p>
+  //   <input name="des" type="text" onChange={props.submit}/>
+  //   <p>Occasion:</p>
+  //   <input name="occ" type="text" onChange={props.submit}/>
+  //   <p>Deadline:</p>
+  //   <input name="deadline" type="text" onChange={props.submit}/>
+  //   <button type="submit" onClick={props.submit}>Submit!</button>
+  // </div>
   )
 }
 
